98c898812c2a819beaf3fdd3c41dee40e5324002
azizbek <pubgmobileazik07@gmail.com>
2024-05-31 21:50:39 +0500

    exam

--- apps/views.py
+++ apps/views.py
@@ -5,7 +5,6 @@ from random import randint
 from django.core.mail import send_mail
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_yasg.openapi import Response
-from rest_framework import status
 from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -39,21 +38,17 @@ class SendVerificationCode(GenericAPIView):
     def post(self, request):
         user_email = request.data.get('email')
 
-        # Generate 6-digit verification code
         verification_code = randint(100000, 999999)
 
         subject = 'Hisob tasdiqlash kodi'
         message = f'Assalomu alaykum!\nSizning tasdiqlash kodingiz: {verification_code}'
         from_email = settings.EMAIL_HOST_USER
 
-        try:
-            send_mail(subject, message, from_email, [user_email])
-            redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
-            redis_conn.set(user_email, verification_code)
-            redis_conn.set(f"{user_email}:timestamp", int(time.time()))
-            return Response({'message': 'Verification code sent successfully'})
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        send_mail(subject, message, from_email, [user_email])
+        redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
+        redis_conn.set(user_email, verification_code)
+        redis_conn.set(f"{user_email}:timestamp", int(time.time()))
+        return Response('Verification code sent successfully')
 
 
 class CheckVerificationCode(GenericAPIView):
@@ -64,7 +59,7 @@ class CheckVerificationCode(GenericAPIView):
         provided_code = request.data.get('code')
 
         if not user_email or not provided_code:
-            return Response({'error': 'Email and verification code are required'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'Email and verification code are required'},)
 
         redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
         stored_code = redis_conn.get(user_email)
@@ -77,8 +72,8 @@ class CheckVerificationCode(GenericAPIView):
                 if current_timestamp - stored_timestamp <= 60:
                     redis_conn.delete(user_email)
                     redis_conn.delete(f"{user_email}:timestamp")
-                    return Response({'message': 'Verification successful'}, status=status.HTTP_200_OK)
+                    return Response('Verification successful',)
 
-            return Response({'error': 'Verification code has expired'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response('Verification code has expired',)
         else:
-            return Response({'error': 'Invalid verification code'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'Invalid verification code'},)
